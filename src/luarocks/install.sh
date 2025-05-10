e14e4579eba9e8c964f2a5af46d1e0661269ef91
Aire-One <aireone@aireone.xyz>
2025-05-10 23:31:29 +0000

    feat: add LuaRocks feature

---
+++ src/luarocks/install.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+
+set -e
+
+apt-get update -y
+apt-get install -y \
+    build-essential \
+    tar \
+    wget
+
+VERSION="${VERSION:-"3.11.1"}"
+
+wget https://luarocks.github.io/luarocks/releases/luarocks-${VERSION}.tar.gz -O - | tar -xzf -
+
+cd luarocks-${VERSION}
+
+./configure --with-lua=/usr/local
+make build
+make install
+
+rm -rf luarocks-${VERSION}
